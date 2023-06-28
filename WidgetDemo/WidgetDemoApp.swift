9e4459289512e3213146886d392a1ec4aeae6d2b
서창열 <kongbaguni@gmail.com>
2023-06-28 09:09:45 +0900

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Changyeol Seo on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
