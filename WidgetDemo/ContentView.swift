720a73d764bebaf2a45fc7cadceb2196fafc8de3
chang-yul, Seo <kongbaguni@gmail.com>
2023-08-11 09:22:24 +0900

    리워드 포인트 수정

--- WidgetDemo/ContentView.swift
+++ WidgetDemo/ContentView.swift
@@ -170,7 +170,7 @@ struct ContentView: View {
             Alert(title: Text("adAlertConfirm_title"),
                   primaryButton: .default(Text("confirm"), action: {
                 ad.showAd { _, _ in
-                    adWatchPoint += 10
+                    adWatchPoint += 50
                     isPresentedImageView = true
                 }
             }), secondaryButton: .cancel())
